b3130c42ee4c08fae7a3e14a489b9b8f8fda338a
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-03-27 20:02:16 -0400

    Update donate.php

--- donate.php
+++ donate.php
@@ -34,28 +34,31 @@ if(isset($_POST['submit']))
 <input type="hidden" name="lc" value="CA">
 <input type="hidden" name="item_name" value="Donation">
 <input type="hidden" name="button_subtype" value="services">
-<input type="hidden" name="no_note" value="0">
-<input type="hidden" name="cn" value="Add special instructions to the seller:">
-<input type="hidden" name="no_shipping" value="2">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="rm" value="1">
+<input type="hidden" name="return" value="http://paypal-sandbox-test.herokuapp.com/success.php">
+<input type="hidden" name="cancel_return" value="http://paypal-sandbox-test.herokuapp.com/failure.php">
 <input type="hidden" name="currency_code" value="CAD">
 <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
 <table>
 <tr><td><input type="hidden" name="on0" value="prices">prices</td></tr><tr><td><select name="os0">
-	<option value="Option1">$5.00 CAD</option>
-	<option value="Option2">$10.00 CAD</option>
-	<option value="Option3">$20.00 CAD</option>
+	<option value="Option 1">Option 1 $5.00 CAD</option>
+	<option value="Option 2">Option 2 $5.00 CAD</option>
+	<option value="Option 3">Option 3 $5.00 CAD</option>
 </select> </td></tr>
 </table>
 <input type="hidden" name="option_select0" value="Option 1">
 <input type="hidden" name="option_amount0" value="5.00">
 <input type="hidden" name="option_select1" value="Option 2">
-<input type="hidden" name="option_amount1" value="10.00">
+<input type="hidden" name="option_amount1" value="5.00">
 <input type="hidden" name="option_select2" value="Option 3">
-<input type="hidden" name="option_amount2" value="20.00">
+<input type="hidden" name="option_amount2" value="5.00">
 <input type="hidden" name="option_index" value="0">
 <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 
 
 </body>

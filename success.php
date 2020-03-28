e462cf8160e0f45decfdc1f90dd2141955c1ae51
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-03-27 21:01:13 -0400

    Update success.php

--- success.php
+++ success.php
@@ -3,7 +3,7 @@
 	$tempamount=intval($_GET['amount']);
 	
 	$ret=mysqli_query($con, "select * from fundraiser where id='$uid'");
-	$row=mysqli_fetch_array($ret)
+	$row=mysqli_fetch_array($ret);
 	$current=$row['current'];
 	$new=$current+$tempamount;
 	$query=mysqli_query($con,"UPDATE fundraiser set current='$new' where id='$uid'");
